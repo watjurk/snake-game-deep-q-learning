e01d0662851c9d6d32c0fd28af16e775a1aa1b04
watjurk <watjurk@gmail.com>
2022-04-07 13:07:33 +0200

    Update brains

--- brain/snake_v2/brain.py
+++ brain/snake_v2/brain.py
@@ -25,32 +25,32 @@ class Brain(q_learning.Brain):
   def build_model(self, learning_rate):
     self.model = keras.Sequential()
 
-    self.model.add(keras.layers.InputLayer(input_shape=(50, 50, 3)))
+    self.model.add(keras.layers.InputLayer(input_shape=(38, 38, 3)))
 
-    self.model.add(keras.layers.Convolution2D(32, kernel_size=(8, 8), activation='relu'))
+    self.model.add(keras.layers.Convolution2D(64, kernel_size=(8, 8), activation='relu'))
     self.model.add(keras.layers.MaxPooling2D(pool_size=(4, 4)))
 
-    self.model.add(keras.layers.Convolution2D(16, kernel_size=(4, 4), activation='relu'))
+    self.model.add(keras.layers.Convolution2D(32, kernel_size=(4, 4), activation='relu'))
     self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
     self.model.add(keras.layers.Flatten())
 
-    self.model.add(keras.layers.Dense(128, activation='relu'))
+    self.model.add(keras.layers.Dense(256, activation='relu'))
 
-    self.model.add(keras.layers.Dense(4, activation='softmax'))
+    self.model.add(keras.layers.Dense(4))
 
     self.model.compile(loss=keras.losses.MeanSquaredError(), optimizer=keras.optimizers.Adam(learning_rate=learning_rate))
 
   def prepare_observation(self, observation):
     assert(observation.shape == (150, 150, 3))
 
-    # if self.last_observations is not None:
-    #   alpha = 0.6
-    #   beta = (1.0 - alpha)
-    #   observation = cv2.addWeighted(observation, alpha, self.last_observations, beta, 0)
-    # self.last_observations = observation
+    if self.last_observations is not None:
+      alpha = 0.6
+      beta = (1.0 - alpha)
+      observation = cv2.addWeighted(observation, alpha, self.last_observations, beta, 0)
+    self.last_observations = observation
 
-    observation = cv2.resize(observation, dsize=(50, 50), interpolation=cv2.INTER_AREA)
+    observation = cv2.resize(observation, dsize=(38, 38), interpolation=cv2.INTER_AREA)
     # self.ui.video.update_stream('brain', observation)
     
     observation = observation / 255.0
