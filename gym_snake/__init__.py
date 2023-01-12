fa911dc6fc208fc5c9b87f6d9f027d945f2f1094
watjurk <watjurk@gmail.com>
2023-01-13 00:11:33 +0100

    fix compatibility issues

--- gym_snake/__init__.py
+++ gym_snake/__init__.py
@@ -3,8 +3,10 @@ from gym.envs.registration import register
 register(
     id='snake-v0',
     entry_point='gym_snake.envs:SnakeEnv',
+    disable_env_checker=True
 )
 register(
     id='snake-plural-v0',
     entry_point='gym_snake.envs:SnakeExtraHardEnv',
+    disable_env_checker=True
 )
