e6951f5c11023284c3132909673e680bab5f0558
Wiktor Jurkiewicz <watjurk@gmail.com>
2024-04-26 16:32:31 +0200

    init gui

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
+import threading
+import time
+
 import torch
+
+import gui
 
 print("Hello world!")
 print("Torch version:", torch.__version__)
@@ -6,3 +11,7 @@ print("Torch version:", torch.__version__)
 print("Torch default device:", torch.get_default_device())
 print("Torch mps available:", torch.backends.mps.is_available())
 print("Torch cuda available:", torch.cuda.is_available())
+
+threading.Thread(target=gui.start).start()
+
+time.sleep(100)
