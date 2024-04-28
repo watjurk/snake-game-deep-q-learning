657419ea6de644bd18f8a6a60e7c77a014e5ef74
Wiktor Jurkiewicz <watjurk@gmail.com>
2024-04-28 23:36:06 +0200

    replace gui with cv2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
-import threading
-import time
-
+import cv2
+import numpy as np
 import torch
-
-import gui
 
 print("Hello world!")
 print("Torch version:", torch.__version__)
@@ -12,6 +9,33 @@ print("Torch default device:", torch.get_default_device())
 print("Torch mps available:", torch.backends.mps.is_available())
 print("Torch cuda available:", torch.cuda.is_available())
 
-threading.Thread(target=gui.start).start()
 
-time.sleep(100)
+def nothing(x):
+    pass
+
+
+# Create a black image, a window
+img = np.zeros((200, 200, 3), np.uint8)
+cv2.namedWindow("image")
+
+cv2.createTrackbar("R", "image", 0, 255, nothing)
+cv2.createTrackbar("G", "image", 0, 255, nothing)
+cv2.createTrackbar("B", "image", 0, 255, nothing)
+
+
+while True:
+    cv2.imshow("image", img)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    r = cv2.getTrackbarPos("R", "image")
+    g = cv2.getTrackbarPos("G", "image")
+    b = cv2.getTrackbarPos("B", "image")
+
+    print(r, g, b)
+
+    img[:] = [b, g, r]
+
+cv2.destroyAllWindows()
